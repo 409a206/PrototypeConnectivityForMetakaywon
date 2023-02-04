8c752bffe7b9f4d5ed4766536c2f3c4ade7e507d
hyungjin <skrt1533@gmail.com>
2023-02-04 22:26:45 +0900

    made working

---
+++ Assets/Scripts/GSpreadSheet/DataForGoogleSpreadSheet.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataForGoogleSpreadSheet : ScriptableObject
+{
+    public string 학번;
+    public string 이름;
+    public string 코인갯수;
+    public string 메달갯수;
+    public string 일시;
+}
